7390566de66489a3e19f486ab268c7e00033d0b8
kamlesh <kamlesh81473@gmail.com>
2025-08-15 18:16:00 +0530

    add new code

--- rest_class/index.js
+++ rest_class/index.js
@@ -2,31 +2,42 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+app.use(express.json());
+
 
 app.use(express.urlencoded({ extended: true}));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
-        usename:"Kamlesh",
+        username:"Kamlesh18.0",
         content:"I love codding"
     },
     {
-        usename:"Rahul",
+        username:"Rahul775",
         content:"Hard work is the key to success"
     },
     {
-        usename:"Rakesh",
+        username:"Rakesh2.88",
         content:"I got selected in TCS"
     },
 ];
 
 app.get("/posts",(req,res)=>{
-    res.send("server woerking well");
+    res.render("index.ejs",{posts});
+});
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, content });
+    res.send("Post request successfully working");
 });
 
 app.listen(port, () => {
